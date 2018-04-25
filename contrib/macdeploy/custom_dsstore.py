efde78d09bc07fd6e36921ef28a33e26cdcf8cb6
EKody <emk4827@gmail.com>
2018-04-25 04:25:05 +0300

    mac deploy fixes

--- contrib/macdeploy/custom_dsstore.py
+++ contrib/macdeploy/custom_dsstore.py
@@ -54,7 +54,7 @@ ds['.']['icvp'] = icvp
 ds['.']['vSrn'] = ('long', 1)
 
 ds['Applications']['Iloc'] = (370, 156)
-ds['Nyerium-Qt.app']['Iloc'] = (128, 156)
+ds['NYERIUM-Qt.app']['Iloc'] = (128, 156)
 
 ds.flush()
 ds.close()
